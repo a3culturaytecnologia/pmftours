79db0cc8f1c722ccd528a0841fb059bcec5cad17
A3 Cultura y Tecnología <aidigitalstudio.pa@gmail.com>
2025-10-05 01:42:32 -0500

    fix: corregir ubicación del service worker y rutas de recursos

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'pmf-tours-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './css/styles.css',
+    './css/kia-animation.css',
+    './js/main.js',
+    './js/car-animation.js',
+    './js/form-handler.js',
+    './js/hero-slider.js',
+    './js/language-switcher.js',
+    './js/mobile-menu.js',
+    './image/logo.png',
+    './image/logo-blanco.png',
+    './site.webmanifest'
+];
+
+// Instalación
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+// Activación
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Estrategia de caché
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cached) => {
+                const networked = fetch(event.request)
+                    .then((response) => {
+                        const cacheCopy = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => cache.put(event.request, cacheCopy));
+                        return response;
+                    })
+                    .catch(() => cached);
+                return cached || networked;
+            })
+    );
+});
