a64dc52ff1eb19df05a244cb32fb8780703cf35e
A3 Cultura y Tecnología <aidigitalstudio.pa@gmail.com>
2025-10-05 01:25:16 -0500

    fix: mejorar service worker con estrategia de caché

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,21 +5,51 @@ const ASSETS = [
     './css/styles.css',
     './css/kia-animation.css',
     './js/main.js',
+    './js/car-animation.js',
+    './js/form-handler.js',
+    './js/hero-slider.js',
+    './js/language-switcher.js',
+    './js/mobile-menu.js',
     './image/logo.png',
     './image/logo-blanco.png',
-    './image/favicon/favicon.svg'
+    './site.webmanifest'
 ];
 
+// Instalación
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => cache.addAll(ASSETS))
     );
+    self.skipWaiting();
 });
 
+// Activación
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Estrategia de caché
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+            .then((cached) => {
+                const networked = fetch(event.request)
+                    .then((response) => {
+                        const cacheCopy = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => cache.put(event.request, cacheCopy));
+                        return response;
+                    })
+                    .catch(() => cached);
+                return cached || networked;
+            })
     );
 });
