bc7b2a02336525656b26768df56bcbddb08d85a1
A3aizprua <aizprua2@gmail.com>
2026-01-06 19:33:27 -0500

    chore: remove backup files and temporary artifacts

--- js/backup/i18n.js
+++
@@ -1,5 +0,0 @@
-function getDataTranslate(el) {
-  if (!el) return null;
-  return (el.dataset && el.dataset.translate) || el.getAttribute('data-translate');
-}
-window.getDataTranslate = getDataTranslate;
